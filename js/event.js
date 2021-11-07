a20199395616620aa21082c79dd6ad72451e6028
Elie Coffec <eliecoffec@gmail.com>
2021-11-07 19:09:09 +0100

    Menu vertical version desktop

--- js/event.js
+++ js/event.js
@@ -1,6 +1,11 @@
 const MENUBURGER = document.querySelector("#picto-menu-burger");
 const MENUPICTO = document.querySelector("#menu-picto");
 const PICTOCLOSE = document.querySelector("#picto-close");
+const PICTOREDUIRE = document.querySelector("#picto-reduire");
+const MENUVERTICAL = document.querySelector('#menu-vertical');
+const PICTOAFFICHER = document.querySelector('#picto-afficher');
+
+console.log(PICTOAFFICHER)
 
 MENUBURGER.addEventListener("click", ()=>{
     MENUPICTO.classList.remove("none");
@@ -10,4 +15,14 @@ MENUBURGER.addEventListener("click", ()=>{
 PICTOCLOSE.addEventListener("click", ()=>{
     MENUPICTO.classList.add("none");
     PICTOCLOSE.classList.add("none");
+});
+
+PICTOREDUIRE.addEventListener("click", ()=>{
+    MENUVERTICAL.classList.add("none");
+    PICTOAFFICHER.classList.remove("none");
+});
+
+PICTOAFFICHER.addEventListener("click", ()=>{
+    MENUVERTICAL.classList.remove("none");
+    PICTOAFFICHER.classList.add("none");
 });
